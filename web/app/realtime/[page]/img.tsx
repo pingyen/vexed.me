679cc7981fa1c432454c26efdf76828d73c79001
Ping-Yen Tsai <pingyen@cloudmosa.com>
2024-07-01 17:30:37 +0800

    web: Refactor realtime/[page]/img.tsx to improve CLS.

--- web/app/realtime/[page]/img.tsx
+++ web/app/realtime/[page]/img.tsx
@@ -11,7 +11,10 @@ export default function Img({ src, alt }: { src: string, alt: string }) {
     const img = new Image();
 
     img.addEventListener('load', () => {
-      current.className = 'block my-2 w-full max-w-sm';
+      current.addEventListener('load', () => {
+        current.className = 'block my-2 w-full max-w-sm';
+      });
+
       current.src = src;
     });
 
@@ -23,5 +26,5 @@ export default function Img({ src, alt }: { src: string, alt: string }) {
   }, [src]);
 
   return error === false &&
-    <img ref={imgRef} alt={alt} className="hidden" />; // eslint-disable-line @next/next/no-img-element
+    <img ref={imgRef} alt={alt} className="invisible h-60" />; // eslint-disable-line @next/next/no-img-element
 }
