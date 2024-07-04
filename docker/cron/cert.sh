7e1092cad1a3b8a94e7090024d7d41ca9b96c065
Ping-Yen Tsai <pingyen@gmail.com>
2024-07-05 04:02:50 +0800

    docker: Refactor certbot.

---
+++ docker/cron/cert.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$(dirname "$(dirname "$(realpath "$0")")")")
+
+docker compose -f docker/compose/base.yml -f docker/compose/prod.yml run --rm certbot \
+  certonly --keep --webroot --webroot-path=/var/www/certbot --email $1 --agree-tos --no-eff-email -d $2
+
+docker compose -f docker/compose/base.yml -f docker/compose/prod.yml exec nginx \
+  nginx -s reload
