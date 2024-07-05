db842550d92964f653ff0d2dadba99a827a169bd
Ping-Yen Tsai <pingyen@gmail.com>
2024-07-06 03:23:15 +0800

    Redirect www.vexed.me to vexed.me.

--- docker/cron/cert.sh
+++ docker/cron/cert.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$(dirname "$(dirname "$(realpath "$0")")")")
 
 docker compose -f docker/compose/base.yml -f docker/compose/prod.yml run --rm certbot \
-  certonly --keep --webroot --webroot-path=/var/www/certbot --email $1 --agree-tos --no-eff-email -d $2
+  certonly --keep --webroot --webroot-path=/var/www/certbot --email $1 --agree-tos --no-eff-email -d $2 -d www.$2
 
 docker compose -f docker/compose/base.yml -f docker/compose/prod.yml exec nginx \
   nginx -s reload
