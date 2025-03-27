d0ef3e52d9414db85945534bd93d2c68db9a85f8
Ping-Yen Tsai <pingyen@gmail.com>
2025-03-27 23:23:37 +0800

    crawler: Update cleanString().

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -19,7 +19,7 @@ interface Source {
   method?: string
 };
 
-const cleanString = (str: string) => str.replace(/[ …\.]/g, '').toLowerCase();
+const cleanString = (str: string) => str.replace(/[ …\.]/g, '').replace(/　/g, ' ').toLowerCase();
 
 const stripCtrlChars = (str: string) => str.replace(/[\u0000-\u001F\u007F-\u009F]/g, '');
 
