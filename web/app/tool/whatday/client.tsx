202f1a1cc20c5911b91013cf54e16c18e52c5426
Ping-Yen Tsai <pingyen@gmail.com>
2025-07-31 14:31:58 +0800

    web: Enhance data input in tool/whatday.

--- web/app/tool/whatday/client.tsx
+++ web/app/tool/whatday/client.tsx
@@ -26,11 +26,10 @@ export default function Client() {
 
     let value = target.value;
 
-    const result = value.match(/(\d{4})(\d{2})(\d{2})/);
+    const result = value.match(/(\d{4})-?(\d{2})-?(\d{2})/);
 
     if (result !== null) {
       value = `${result[1]}-${result[2]}-${result[3]}`;
-      target.value = value;
     }
 
     const time = Date.parse(value);
